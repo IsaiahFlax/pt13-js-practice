43f9840eab25853fa9bf41e8e37cf815390065a5
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-12 00:04:11 -0700

    Update index.js
    done and done and done

--- index.js
+++ index.js
@@ -8,23 +8,53 @@ function fooBarBanana(arr, cb) {
 // Return a list of all character names
 // expected input: array
 // expected output: array
-function listCharacterNames(/** your code here */) {
-  // your code here
+function listCharacterNames(array) {
+  let names = [];
+  for (let i = 0; i < array.length; i++) {
+    names.push(array[i].character.name);
+    
+  }
+// names.push(array[0].character.name);
+// names.push(array[1].character.name);
+// names.push(array[2].character.name);
+// names.push(array[3].character.name);
+// names.push(array[4].character.name);
+// names.push(array[5].character.name);
+// names.push(array[6].character.name);
+// names.push(array[7].character.name);
+// names.push(array[8].character.name);
+// names.push(array[9].character.name);
+return names;
+  
 }
-
 // return a count of all characters where 'dead' is true
 // expected input: array
 // expected output: integer
-function findOnlyDeadCharacters(/** your code here */) {
-  // your code here
-}
+function findOnlyDeadCharacters(array) {
+  let names2 = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].dead === true) {
+      names2.push(array[i].character.name);
+      
+    }
+  }
+return names2;
+  }
 
 // Return the character name of the player who has completed the most challenges
 // expected input: array
 // expect output: string "{ name } has completed the most challenges!"
-function mostChallengesCompleted(/** your code here */) {
-  // your code here
+function mostChallengesCompleted(array) {
+  let names3 = [];
+  for (let i = 0; i < array.length; i++) {
+      names3.push(array[i].account.challenges.total);
+      
+    }
+    console.log (names3);
+    let winner = names3.indexOf(Math.max(...names3));
+    return `${(array[winner].character.name)} has completed the most challenges!`;
 }
+
 
 ////////////////// TESTS! IGNORE ME! //////////////////
 ////////////////// TESTS! IGNORE ME! //////////////////
