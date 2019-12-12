3829eb5244b53b2b098993461a6e52f393c50301
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-12 08:38:34 -0700

    Update index.js
    tried using map instead of for in

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ function fooBarBanana(arr, cb) {
 // expected input: array
 // expected output: array
 function listCharacterNames(array) {
-  let names = [];
-  for (let i = 0; i < array.length; i++) {
-    names.push(array[i].character.name);
-    
+  // let names = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   names.push(array[i].character.name);
+    let names = array.map((val, i, array) => {
+      return val.character.name;
+    });
+    return names;
   }
 // names.push(array[0].character.name);
 // names.push(array[1].character.name);
@@ -24,35 +27,51 @@ function listCharacterNames(array) {
 // names.push(array[7].character.name);
 // names.push(array[8].character.name);
 // names.push(array[9].character.name);
-return names;
+// return names;
   
-}
+
 // return a count of all characters where 'dead' is true
 // expected input: array
 // expected output: integer
 function findOnlyDeadCharacters(array) {
-  let names2 = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].dead === true) {
+   let names2 = [];
+   for (let i = 0; i < array.length; i++) {
+     if (array[i].dead === true) {
       names2.push(array[i].character.name);
       
     }
   }
-return names2;
-  }
+ return names2;
+}
+// let nameArrayDead = array.map ((v, i, a) => {
+//   if (v.dead === true) {
+//     return v.character.name;
+//   }
+// });
+// //console.log(nameArrayDead); ---> [
+//   undefined, undefined,
+//   undefined, undefined,
+//   undefined, 'xVisco',
+//   'xdukanx', undefined,
+//   undefined, 'Dollarmaker'
+// ]
+//   }
 
 // Return the character name of the player who has completed the most challenges
 // expected input: array
 // expect output: string "{ name } has completed the most challenges!"
 function mostChallengesCompleted(array) {
-  let names3 = [];
-  for (let i = 0; i < array.length; i++) {
-      names3.push(array[i].account.challenges.total);
+  // let names3 = [];
+  // for (let i = 0; i < array.length; i++) {
+  //     names3.push(array[i].account.challenges.total);
       
-    }
-    console.log (names3);
-    let winner = names3.indexOf(Math.max(...names3));
-    return `${(array[winner].character.name)} has completed the most challenges!`;
+  //   }
+  //   console.log (names3);
+  //   let winner = names3.indexOf(Math.max(...names3));
+  //   return `${(array[winner].character.name)} has completed the most challenges!`;
+let newArray3 = array.map((v, i, a) => {
+  Math.max(...v.account.challenges.total);
+})
 }
 
 
